e5f53ad1b97354454604d260bbd2c55543846f5d
Adam Watkins <adam@stupidpupil.co.uk>
2021-08-04 09:41:41 +0100

    Rework to producing departure slice

--- R/produce_departures_slice.R
+++ R/produce_departures_slice.R
@@ -1,22 +1,18 @@
 
-produce_ret2 <- function(){
+produce_departures_slice <- function(start_time, fifteen_minute_intervals=(6*4)-1){
   r5r_core <- setup_r5("data")
 
   lsoa_trip_points <- st_read("data/lsoa11_nearest_road_points.geojson") %>%
   mutate(id = LSOA11CD)
 
-  target_arrival_time <- lubridate::ymd_hms("2021-07-20 20:05:00", tz="Europe/London")
-  departure_time_offsets <- 1:(6*4)*-15
+  departure_time_offsets <- (0:(fifteen_minute_intervals-1))*15
 
   ret <- tibble(from_id=character(0), to_id=character(0))
-  origins <- lsoa_trip_points
 
   for (offset in departure_time_offsets) {
 
-    departure_time <- target_arrival_time + lubridate::dminutes(offset)
+    departure_time <- start_time + lubridate::dminutes(offset)
     print(departure_time)
-
-    departure_time %>% as.character() %>% write("departure_time")
 
     ttm <- travel_time_matrix(r5r_core = r5r_core,
       origins = lsoa_trip_points,
@@ -24,13 +20,11 @@ produce_ret2 <- function(){
       mode = c('WALK', 'TRANSIT'),
       departure_datetime = departure_time,
       max_walk_dist = 3000,
-      max_trip_duration = max(-offset,120),
+      max_trip_duration = (6*60),
       verbose = FALSE
       ) 
 
-    print("here")
-
-    ttm <-ttm %>%
+    ttm <- ttm %>%
     rename(
       from_id = fromId, to_id = toId,
       travel_time_minutes = travel_time) %>%
@@ -43,9 +37,14 @@ produce_ret2 <- function(){
     bind_rows(ttm) %>%
     group_by(from_id, to_id, arrival_time) %>% arrange(departure_time) %>% slice_tail() %>% ungroup()
 
-    saveRDS(ret, "ret.rds")
-
+    ttm <- NULL
   }
 
+  filename = paste0(
+    "departures_slice_", 
+    start_time %>% strftime("%Y-%m-%dT%H-%M"),
+    "_", fifteen_minute_intervals, "x15.rds")
+
+  ret %>% saveRDS(paste0("output/", filename), compress="xz")
 
 }
