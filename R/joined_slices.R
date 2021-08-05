136b23dbd348038903dd3a4fe8eadc3f94b5bacc
Adam Watkins <adam@stupidpupil.co.uk>
2021-08-05 13:14:42 +0100

    Narrow selection of RDSs

--- R/joined_slices.R
+++ R/joined_slices.R
@@ -1,3 +1,3 @@
 joined_slices <- function(){
-  do.call(bind_rows, lapply(list.files("output/", pattern="*.rds", full.names=TRUE), readRDS))
+  do.call(bind_rows, lapply(list.files("output/", pattern="departures_slice_.+\\.rds", full.names=TRUE), readRDS))
 }
