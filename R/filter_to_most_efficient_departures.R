b0d7dd810641f212455541f48c8332736ea157db
Adam Watkins <adam@stupidpupil.co.uk>
2021-08-05 13:14:30 +0100

    Slightly document

--- R/filter_to_most_efficient_departures.R
+++ R/filter_to_most_efficient_departures.R
@@ -1,3 +1,6 @@
+# Removes all but the latest known departure for each known arrival time 
+# (for each from_id/to_id pair).
+#
 filter_to_most_efficient_departures <- function(journeys){
   journeys %>%
     arrange(from_id, to_id, arrival_time, departure_time) %>%
