dc41788ad419964a1bdc42f47ae3f079b9906c60
Adam Watkins <adam@stupidpupil.co.uk>
2021-11-25 14:50:47 +0000

    Try to create quantile-based matrix

---
+++ R/write_depart_between_quantile_matrix.R
@@ -0,0 +1,62 @@
+write_depart_between_quantile_matrix <- function(journeys, depart_after, depart_by, q){
+  journeys <- journeys %>%
+    filter(departure_time >= depart_after, departure_time <= depart_by)
+
+  # TODO: Define departure_time levels
+  # At the moment, we just hope that they're all present
+  # for at least one origin-destination pair
+
+  print("Here")
+  print(nrow(journeys))
+
+  print("Here2")
+  print(nrow(journeys))
+  print(journeys$departure_time %>% unique)
+
+  # journeys <- journeys %>%
+  # group_by(from_id, to_id) %>%
+  # summarise(travel_time_minutes = quantile(travel_time_minutes,q)) %>% ungroup %>%
+  # mutate(travel_time_minutes = if_else(travel_time_minutes > 7*60, NA_integer_, as.integer(travel_time_minutes)))
+
+  journeys <- journeys %>% 
+    group_by(from_id, to_id)
+    summarise(travel_time_minutes = quantile(travel_time_minutes,q)) %>%
+    ungroup() %>%
+    collect()
+
+
+  journeys <- mutate(journeys,
+    travel_time_minutes = if_else(travel_time_minutes > 7*60, NA_integer_, as.integer(travel_time_minutes)))
+
+  print("Here3")
+  print(nrow(journeys))
+
+  csv_name <- paste0(
+    "output/depart_between_", 
+    depart_after %>% strftime("%a%H%M-", tz="Europe/London"),
+    depart_by %>% strftime("%a%H%M", tz="Europe/London"), 
+    "_p", as.integer(q*100), "_public.csv")
+
+  journeys %>%
+    pivot_wider(names_from="to_id", values_from="travel_time_minutes") %>%
+    write_csv(csv_name, na="")
+
+  add_matrix_to_index_json(list(
+    path = basename(csv_name),
+    units = "minutes",
+    name = paste0(
+      "Public transport, depart", 
+      depart_by %>% strftime("%H:%M-", tz="Europe/London"),  
+      depart_after %>% strftime("%H:%M %a %d %B %Y", tz="Europe/London"),
+      " p", as.integer(q*100)
+      ),
+    mode = "public_transport",
+    time_ref_type = "depart_between_quantile",
+    time_ref_start = depart_after %>% lubridate::format_ISO8601(usetz = TRUE),
+    time_ref_end   = depart_by %>% lubridate::format_ISO8601(usetz = TRUE),
+    quantile = q,
+    license = "ODbL-1.0",
+    license_ref = "https://github.com/stupidpupil/wales_ish_r5r_runner/tree/matrix-releases#licence"
+  ))
+
+}
