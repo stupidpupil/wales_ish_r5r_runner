734b9a76f273516238825390f4d0463c20561e43
Adam Watkins <adam@stupidpupil.co.uk>
2021-11-25 14:58:43 +0000

    Let duckdb use a temp directory

--- R/joined_slices.R
+++ R/joined_slices.R
@@ -1,5 +1,6 @@
 joined_slices <- function(){
   con <- dbConnect(duckdb::duckdb())
+  dbSendQuery(con, paste0("PRAGMA temp_directory='", tempdir() ,"/duckdb'"))
   dbSendQuery(con, "CREATE VIEW departures AS SELECT * FROM parquet_scan('output/*.parquet');")
   tbl(con, "departures")
 }
